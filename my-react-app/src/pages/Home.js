4b9a72a46a19d3e5dfcfa75bd92f73bc54585e1c
desmond1405 <desmondantony14@gmail.com>
2024-02-14 23:35:22 +0530

    chatbot added to baleno

--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -3,29 +3,13 @@ import myImage from '../components/images/img2.png';
 import myImage2 from '../components/images/webpage2sec.png';
 import myImage3 from '../components/images/webpagefooter.png';
 import Chatbot from '../components/Chatbot';
-import React, { useState, useEffect } from 'react';
+
 import { RiCustomerService2Fill } from "react-icons/ri";
 
 import Navbar from '../components/Navbar';
 import Cars from '../components/Cars';
-export default function Home() {
-    const [show, setShow] = useState(false);
-    useEffect(() => {
-        // Scroll to the top when the App component mounts
-        window.scrollTo(0, 0);
+export default function Home({visible,close}) {
     
-        // Set show state to true after 5 seconds
-        const timeoutId = setTimeout(() => {
-          setShow(true);
-        }, 3000);
-    
-        // Clear the timeout when the component unmounts to avoid memory leaks
-        return () => clearTimeout(timeoutId);
-      }, []);
-    
-      const toggleShow = () => {
-        setShow(!show);
-      };
   return (
     <div>
       <>
@@ -38,14 +22,14 @@ export default function Home() {
     
     <button
       className={`fixed bttt bottom-10 right-10 z-50 bg-[#002b57] text-white rounded-full p-2 hover:bg-gray-900`}
-      onClick={toggleShow}
+      onClick={close}
     > 
 
 <RiCustomerService2Fill className='w-[55px] h-[55px] '/>
       
     </button>
 
-    <Chatbot visible={show} close={toggleShow} />
+    <Chatbot visible={visible} close={close} />
     <img className='w-full mt-[6px]' src={myImage3} alt='hero' />
 
   </>
